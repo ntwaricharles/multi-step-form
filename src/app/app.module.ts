4408eaffa3a52f4ce09add8d44efff0ae8680f2a
ntwaricharles <charles.ntwari@amalitechtraining.org>
2024-07-11 17:35:00 +0200

    multi-step-form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PersonalInfoFormComponent } from './components/personal-info-form/personal-info-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonalInfoFormComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/personal-info-form/personal-info-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-personal-info-form',
+  templateUrl: './personal-info-form.component.html',
+  styleUrl: './personal-info-form.component.css'
+})
+export class PersonalInfoFormComponent {
+
+}
