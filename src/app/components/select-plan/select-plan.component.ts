d64eb52ab83836c0b064da5bfc75444a661f8f27
ntwaricharles <charles.ntwari@amalitechtraining.org>
2024-07-13 12:35:29 +0200

    updated select plan component

--- src/app/components/select-plan/select-plan.component.ts
+++ src/app/components/select-plan/select-plan.component.ts
@@ -1,4 +1,5 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, Input } from '@angular/core';
+import { PricingService } from '../../pricing.service';
 
 @Component({
   selector: 'app-select-plan',
@@ -6,16 +7,23 @@ import { Component, EventEmitter, Output } from '@angular/core';
   styleUrls: ['./select-plan.component.css'],
 })
 export class SelectPlanComponent {
+  @Input() plan: string = 'arcade';
+  @Input() period: boolean = false;
   @Output() previousStep = new EventEmitter<void>();
   @Output() nextStep = new EventEmitter<void>();
+  @Output() planChange = new EventEmitter<string>();
+  @Output() periodChange = new EventEmitter<boolean>();
 
-  plan: string = 'arcade';
-  period: boolean = false;
-  monthlyPrices: string[] = ['$9/mo', '$12/mo', '$15/mo'];
-  yearlyPrices: string[] = ['$90/yr', '$120/yr', '$150/yr'];
+  constructor(private pricingService: PricingService) {}
 
   togglePeriod() {
     this.period = !this.period;
+    this.periodChange.emit(this.period);
+  }
+
+  selectPlan(plan: string) {
+    this.plan = plan;
+    this.planChange.emit(this.plan);
   }
 
   goBack() {
@@ -24,5 +32,9 @@ export class SelectPlanComponent {
 
   goToNextStep() {
     this.nextStep.emit();
+  }
+
+  getPlanPrice(plan: string) {
+    return this.pricingService.getPlanPrice(this.period, plan);
   }
 }
